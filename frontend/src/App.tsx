87e0c2ce526fc6872b33956809383dd5d7cb0383
omer5yerushalmi <omery2700@gmail.com>
2025-03-20 21:53:46 +0200

    add logout button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,35 @@
-    import { ThemeProvider, CssBaseline } from '@mui/material';
-    import { createTheme } from '@mui/material/styles';
-    import AuthComponent from './components/LoginPage';
-    import HomePage from './components/HomePage';
-    import { useState, useEffect } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import AuthComponent from './components/LoginPage';
+import HomePage from './components/HomePage';
+import { useState, useEffect } from 'react';
 
 
-    const theme = createTheme();
+const theme = createTheme();
 
-    function App() {
-        const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-        const [userEmail, setUserEmail] = useState<string | null>(null);
+function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [userEmail, setUserEmail] = useState<string | null>(null);
 
-        useEffect(() => {
-            // Check if access token exists in localStorage
-            const token = localStorage.getItem('accessToken');
-            const storedEmail = localStorage.getItem('userEmail');
-            setIsAuthenticated(!!token);
-            if (storedEmail) 
-                setUserEmail(storedEmail);
-        }, []);
+    useEffect(() => {
+        // Check if access token exists in localStorage
+        const token = localStorage.getItem('accessToken');
+        const storedEmail = localStorage.getItem('userEmail');
+        setIsAuthenticated(!!token);
+        if (storedEmail) 
+            setUserEmail(storedEmail);
+    }, []);
 
-        return (
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                {isAuthenticated ? (
-                <HomePage userEmail={userEmail}/>
-            ) : (
-                <AuthComponent setIsAuthenticated={setIsAuthenticated} setUserEmail={setUserEmail}/>
-            )}
-            </ThemeProvider>
-        );
-    }
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {isAuthenticated ? (
+            <HomePage userEmail={userEmail}/>
+        ) : (
+            <AuthComponent setIsAuthenticated={setIsAuthenticated} setUserEmail={setUserEmail}/>
+        )}
+        </ThemeProvider>
+    );
+}
 
-    export default App; 
+export default App; 
